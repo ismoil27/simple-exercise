bf983b2e10b39d24e691c9b2f82fe00b7ed6d933
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-07-21 16:57:31 +0900

    db added but should check later

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,3 +1,27 @@
 const express = require("express");
 
 const db = require("../utils/database");
+
+module.exports = class Product {
+  constructor(title, snippet, body) {
+    this.title = title;
+    this.snippet = snippet;
+    this.body = body;
+    //   this.price = price;
+  }
+
+  save() {
+    return db.execute(
+      "INSERT INTO blogs (title, snippet, body) VALUES (?, ?, ?, ?)",
+      [this.title, this.snippet, this.body]
+    );
+  }
+
+  static deleteById(id) {}
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM blogs");
+  }
+
+  static findById(id, cb) {}
+};
